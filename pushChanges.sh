0e7f9c3330c6bc4155ba9f880983ce51cd299ea7
Vff-Group <velvet.fierce2020@gmail.com>
2023-10-17 11:12:16 +0000

    hi

--- pushChanges.sh
+++ pushChanges.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Colors for messages
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'  # No color
+
 # GitHub credentials
 USERNAME="Vff-Group"
 PASSWORD="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"
@@ -9,28 +14,28 @@ REPO_URL="https://github.com/Vff-Group/vff_website.git"
 
 # Function to handle errors
 handle_error() {
-    echo "Error: $1"
+    echo -e "${RED}Error: $1${NC}"
     exit 1
 }
 
 # Pull new changes
-echo "Pulling new changes..."
+echo -e "${GREEN}Pulling new changes...${NC}"
 git pull || handle_error "Failed to pull changes."
 
 # Prompt for commit message
 read -p "Enter commit message: " COMMIT_MSG
 
 # Add changes
-echo "Adding changes..."
+echo -e "${GREEN}Adding changes...${NC}"
 git add . || handle_error "Failed to add changes."
 
 # Commit changes
-echo "Committing changes..."
+echo -e "${GREEN}Committing changes...${NC}"
 git commit -m "$COMMIT_MSG" || handle_error "Failed to commit changes."
 
 # Push changes
-echo "Pushing changes..."
+echo -e "${GREEN}Pushing changes...${NC}"
 #git push https://$USERNAME:$PASSWORD@$REPO_URL || handle_error "Failed to push changes."
 git push -u origin master
-echo "Operation completed successfully."
+echo -e "${GREEN}Operation completed successfully.${NC}"
 
