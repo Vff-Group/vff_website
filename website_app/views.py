e29f171343a67cd79ff764ed5924257633d60558
Vff-Group <velvet.fierce2020@gmail.com>
2023-10-17 17:41:40 +0530

    active tab changes

--- website_app/views.py
+++ website_app/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    current_url = request.get_full_path()
+    return render(request,"index.html",{'current_url': current_url})
 
 def about_us(request):
-    return render(request,"about_us.html")
+    current_url = request.get_full_path()
+    return render(request,"about_us.html",{'current_url': current_url})
 
 def services(request):
-    return render(request,"services.html")
+    current_url = request.get_full_path()
+    return render(request,"services.html",{'current_url': current_url})
 
 def contactus(request):
-    return render(request,"contactus.html")
+    current_url = request.get_full_path()
+    return render(request,"contactus.html",{'current_url': current_url})
 
 def privacy(request):
-    return render(request,"privacy.html")
+    current_url = request.get_full_path()
+    return render(request,"privacy.html",{'current_url': current_url})
