5662be3615fe9a9a145142f60a90e80d12e3cf05
Vff-Group <velvet.fierce2020@gmail.com>
2023-10-28 20:37:23 +0530

    s

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -968,7 +968,7 @@ def delivery_accept(request,order_id,delivery_boy_id):
                 return redirect('dashboard_app:all_orders')
         except Exception as e:
             print(e)
-    return render(request,'order_pages/all_assigning_delivery_boy.html')
+        return redirect('dashboard_app:all_orders')
 #Assign Delivery Boy to Order ID
 def assigned_delivery_boy(request,orderid):
     error_msg = ""
