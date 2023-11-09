8e3ad76451bc99525426d4ab14aeb1519fe57119
Vff-Group <velvet.fierce2020@gmail.com>
2023-11-09 18:49:43 +0530

    s

---
+++ dashboard_app/static/js/firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+importScripts('https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.5/firebase-messaging.js');
+
+// Your Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB377d4_AFRtoEIjpN2Puf3CYwe-I9dCGE",
+    authDomain: "vff-group-b185c.firebaseapp.com",
+    projectId: "vff-group-b185c",
+    storageBucket: "vff-group-b185c.appspot.com",
+    messagingSenderId: "711189707453",
+    appId: "1:711189707453:web:3813986a11e36f830b55d4",
+    measurementId: "G-YYEJHD2GJ1"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve Firebase Messaging object.
+const messaging = firebase.messaging();
+
+// Customize notification behavior when the app is in the background
+messaging.setBackgroundMessageHandler((payload) => {
+  console.log('Background Message:', payload);
+  // Customize the behavior when the app is in the background
+  // You might want to show a notification using the Notification API
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
