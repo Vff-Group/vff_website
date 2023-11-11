f832f8c12c0342a65c5c297e77089927b8ae70ff
Vff-Group <velvet.fierce2020@gmail.com>
2023-11-11 21:48:44 +0530

    s

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -1922,12 +1922,10 @@ def all_main_branches(request):
         return redirect('dashboard_app:login')
     error_msg = "No Branches Created Till Now"
     branch_id = request.session.get('branchid')
-    owner_id = request.session.get('userid')
-    filter = ''
-    if branch_id:
-        filter = "and branchtbl.owner_id='"+str(owner_id)+"'"
+    admin_usrid = request.session.get('userid')
     
-    query = "select branchid,branch_name,owner_name,branchtbl.address,branch_type,creation_date,branchtbl.epoch,status,branchtbl.city,branchtbl.state,mobile_no,usertbl.address,usertbl.city,usertbl.pincode,branchtbl.pincode,profile_img,device_token,owner_id from vff.usertbl,vff.branchtbl where branchtbl.owner_id=usertbl.usrid "+filter+""
+    
+    query = "select admintbl.branchid,branch_name,owner_name,branchtbl.address,branch_type,creation_date,branchtbl.epoch,branchtbl.status,branchtbl.city,branchtbl.state,mobile_no,usertbl.address,usertbl.city,usertbl.pincode,branchtbl.pincode,profile_img,device_token,owner_id from vff.admintbl,vff.usertbl,vff.branchtbl where branchtbl.owner_id=usertbl.usrid and admintbl.usrid=usertbl.usrid and admintbl.usrid=branchtbl.owner_id and admintbl.branchid!='-1'  and admintbl.usrid='"+str(admin_usrid)+"'"
     
     query_result = execute_raw_query(query)
     
