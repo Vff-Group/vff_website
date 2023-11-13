4b273275b92f76e958850db90b8a2ff7104abcc0
Vff-Group <velvet.fierce2020@gmail.com>
2023-11-13 12:07:55 +0530

    d

--- dashboard_app/static/js/plugins-init/select2-init.js
+++ dashboard_app/static/js/plugins-init/select2-init.js
@@ -48,40 +48,40 @@
 
 
   //ajax remote data
-  $(".js-data-example-ajax").select2({
-      width: "100%",
-      ajax: {
-        url: "https://api.github.com/search/repositories",
-        dataType: 'json',
-        delay: 250,
-        data: function (params) {
-          return {
-            q: params.term, // search term
-            page: params.page
-          };
-        },
-        processResults: function (data, params) {
-          // parse the results into the format expected by Select2
-          // since we are using custom formatting functions we do not need to
-          // alter the remote JSON data, except to indicate that infinite
-          // scrolling can be used
-          params.page = params.page || 1;
+  // $(".js-data-example-ajax").select2({
+  //     width: "100%",
+  //     ajax: {
+  //       url: "https://api.github.com/search/repositories",
+  //       dataType: 'json',
+  //       delay: 250,
+  //       data: function (params) {
+  //         return {
+  //           q: params.term, // search term
+  //           page: params.page
+  //         };
+  //       },
+  //       processResults: function (data, params) {
+  //         // parse the results into the format expected by Select2
+  //         // since we are using custom formatting functions we do not need to
+  //         // alter the remote JSON data, except to indicate that infinite
+  //         // scrolling can be used
+  //         params.page = params.page || 1;
     
-          return {
-            results: data.items,
-            pagination: {
-              more: (params.page * 30) < data.total_count
-            }
-          };
-        },
-        cache: true
-      },
-      placeholder: 'Search for a repository',
-      escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
-      minimumInputLength: 1,
-      templateResult: formatRepo,
-      templateSelection: formatRepoSelection
-    });
+  //         return {
+  //           results: data.items,
+  //           pagination: {
+  //             more: (params.page * 30) < data.total_count
+  //           }
+  //         };
+  //       },
+  //       cache: true
+  //     },
+  //     placeholder: 'Search for a repository',
+  //     escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+  //     minimumInputLength: 1,
+  //     templateResult: formatRepo,
+  //     templateSelection: formatRepoSelection
+  //   });
 
     function formatRepo (repo) {
       if (repo.loading) {
