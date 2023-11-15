37010603626cd0f19be50a9ae200b3d892bc3a2b
Vff-Group <velvet.fierce2020@gmail.com>
2023-11-15 10:12:58 +0530

    notification

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -22,6 +22,14 @@ from PIL import Image  # Pillow library for image processing
 # Create your views here.
 serverToken="AAAApZY1ur0:APA91bHsk-e3OC5R2vqO7dD0WZp7ifULNzqrUPnQu07et7RLFMWWcwOqY9Bl-9YQWkuXUP5nM7bVMgMP-qKISf9Jcf2ix9j7oOkScq9-3BH0hfCH3nIWgkn4hbnmSLyw4pmq66rMZz8R"
 
+# Initialize Firebase Admin SDK
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+cred = credentials.Certificate('path/to/serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
+
+
 # def sendFMCMsg(deviceToken,msg,title,data):
 #     global serverToken
 #     deviceToken=deviceToken.replace('__colon__',':')
