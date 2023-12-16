958613525042294324fd933eb510c8732501c8e2
Vff-Group <velvet.fierce2020@gmail.com>
2023-12-16 12:10:18 +0530

    s

--- website_app/views.py
+++ website_app/views.py
@@ -28,13 +28,13 @@ def contact_form_submit(request):
     if request.method == 'POST':
         name = request.POST.get('w3lName')
         email = request.POST.get('email')
-        phone = request.POST.get('phone')  # Ensure the correct input name for phone
+        
         message = request.POST.get('w3lMessage')
-        print(f'phone:::{message}')
+        print(f'Name: {name}\nEmail: {email}\nMessage: {message}')
         # Send email using configured settings
         send_mail(
             'Subject',
-            f'Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}',
+            f'Name: {name}\nEmail: {email}\nMessage: {message}',
             email,  # Replace with your email ID as the sender
             ['info@vff-group.com'],  # Replace with the recipient's email
             fail_silently=False,
