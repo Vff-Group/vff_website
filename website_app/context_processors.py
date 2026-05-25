8087be972081e7002611a970c759097574cc26e1
mohammed786-svg <shahidmaniyar888@gmail.com>
2026-05-25 19:38:58 +0530

    new changes

--- website_app/context_processors.py
+++ website_app/context_processors.py
@@ -2,8 +2,19 @@ from django.templatetags.static import static
 
 from .laundry_images import LAUNDRY_IMAGE_FILES
 
+LOGO_PATH = "img/logos/logo_new.png"
+
+VFF_VIDEOS = [
+    "videos/vff_video_1.mp4",
+    "videos/vff_video_2.mp4",
+    "videos/vff_video_3.mp4",
+    "videos/vff_video_4.mp4",
+]
+
 
 def laundry_media(request):
     return {
-        "img": {key: static(path) for key, path in LAUNDRY_IMAGE_FILES.items()}
+        "img": {key: static(path) for key, path in LAUNDRY_IMAGE_FILES.items()},
+        "logo_url": static(LOGO_PATH),
+        "vff_videos": [static(path) for path in VFF_VIDEOS],
     }
